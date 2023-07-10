657162a86cc044968814f179cbde5df58673da59
John Wason <wason@wasontech.com>
2023-07-10 00:43:38 -0400

    Fix robdef clone in robotraconteur-companion

--- ports/robotraconteur-companion/portfile.cmake
+++ ports/robotraconteur-companion/portfile.cmake
@@ -8,9 +8,9 @@ vcpkg_from_github(
 
 vcpkg_from_github(
     OUT_SOURCE_PATH ROBDEF_SOURCE_PATH
-    REPO robotraconteur/robotraconteur_companion
-    REF v0.3.0
-    SHA512 6ac59b8a9aa81a34649cd600a5ddb2fcfaecbf2cc28dc5a86391e3876bd9678d85b4f34deded438a20c8affc0631aa540bd99c1366f9e13f7c88216066fd930d
+    REPO robotraconteur/robotraconteur_standard_robdef
+    REF group1-v1
+    SHA512 84724717e57c6e7ceefa957a8d94ee68db189e9a114564662d37b16a307735feea2a01c5622140118f537e6c084437d4bf11d0eb1e015b475fb3b636ed5009aa
     HEAD_REF master
 )
 

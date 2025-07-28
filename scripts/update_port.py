9d429d58f5abead35ef8fa8def9d5ba24fbdeb06
John Wason <wason@wasontech.com>
2025-07-28 02:00:56 -0400

    Improve update_port.py

--- scripts/update_port.py
+++ scripts/update_port.py
@@ -61,9 +61,9 @@ def main():
     vcpkg_json2 = re.sub(r'("version-semver"\s*\:\s*)"([^"]+)"', f'\\g<1>"{vcpkg_ver}"', vcpkg_json, count=1)
     print(vcpkg_json2)
 
-    portfile2 = re.sub(r"(vcpkg_from_github\([\w\s\/.\-]*\sREPO\s+)([\w]+\/[\w]+)",f"\\g<1>{repo}", portfile)
-    portfile3 = re.sub(r"(vcpkg_from_github\([\w\s\/.\-]*\sREF\s+)([\w\-.]+)",f"\\g<1>{tag_name}", portfile2)
-    portfile4 = re.sub(r"(vcpkg_from_github\([\w\s\/.\-]*\sSHA512\s+)([A-Za-z0-9]+)",f"\\g<1>{tarball_sha512}", portfile3)
+    portfile2 = re.sub(r"(vcpkg_from_github\([\w\s\/.\-]*\sREPO\s+)([\w]+\/[\w]+)",f"\\g<1>{repo}", portfile, count=1)
+    portfile3 = re.sub(r"(vcpkg_from_github\([\w\s\/.\-]*\sREF\s+)([\w\-.\{\}\$\"]+)",f"\\g<1>{tag_name}", portfile2, count=1)
+    portfile4 = re.sub(r"(vcpkg_from_github\([\w\s\/.\-\{\}\$\"]*\sSHA512\s+)([A-Za-z0-9]+)",f"\\g<1>{tarball_sha512}", portfile3, count=1)
     print(portfile4)
 
     with open(f"ports/{port_name}/vcpkg.json", "w") as f:
